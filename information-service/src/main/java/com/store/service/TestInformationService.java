10450cbbc798b14261e406b9cbca8c0c5101e050
Artem Makovskyy <photomak79@gmail.com>
2025-11-07 15:52:32 +0200

    add module information service

---
+++ information-service/src/main/java/com/store/service/TestInformationService.java
@@ -0,0 +1,11 @@
+package com.store.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestInformationService {
+
+    public String getTestInformation(){
+        return "test";
+    }
+}
--- web/src/main/java/com/store/controller/TestController.java
+++ web/src/main/java/com/store/controller/TestController.java
@@ -1,12 +1,20 @@
 package com.store.controller;
 
+import com.store.service.TestInformationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final TestInformationService testInformationService;
+
     @GetMapping
-    public String test(){return "test";}
+    public String test() {
+        return testInformationService.getTestInformation();
+    }
 }
